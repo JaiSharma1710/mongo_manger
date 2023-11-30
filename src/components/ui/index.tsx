b7d2a2fa0576e570a6eeb02307b298beda35f49e
Jai Sharma <jai.sharma@universityliving.com>
2023-11-30 23:57:14 +0530

    fix build break

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -2,6 +2,5 @@ import DropDown from './DropDown';
 import Logo from './Logo';
 import ModalComponent from './ModalComponent';
 import SelectComponent from './Select';
-import Loading from './Loading';
 
-export { DropDown, Logo, ModalComponent, SelectComponent, Loading };
+export { DropDown, Logo, ModalComponent, SelectComponent };
